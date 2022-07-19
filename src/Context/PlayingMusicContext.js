c309f596d10494cdd13813c7919197bc1d14a582
hoangduc1104 <hoangduc91nda@gmail.com>
2022-07-19 09:54:18 +0700

    fix playlist user

--- src/Context/PlayingMusicContext.js
+++ src/Context/PlayingMusicContext.js
@@ -154,7 +154,8 @@ const PlayingMusicProvider = ({ children }) => {
 
   const logoutRequest = () =>{
     setLogin(false)
-    setCurrentUser(false)
+    setCurrentUser(null)
+    setPlaylistUser([])
     localStorage.setItem("currentUser", null);
   }
 
