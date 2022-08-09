412df5e6cc24afd321920888028eed0ddef760bb
tranphuquy19 <tranphuquy19@gmail.com>
2022-08-09 10:08:53 +0000

    update dockerfile

--- src/API/apiConfig.js
+++ src/API/apiConfig.js
@@ -1,5 +1,7 @@
 const apiConfig = {
-    baseUrl: 'http://localhost:8080/api/'
+    // baseUrl: 'http://localhost:8080/api/'
+    // baseUrl: 'http://10.8.0.14:8080/api/'
+    baseUrl: process.env.REACT_APP_API_URL
 }
 
 export default apiConfig;
