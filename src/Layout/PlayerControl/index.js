79cb00e50e3690dee4611ddf4a40dfb5a0e6ef29
giabaolaptrinh <giabaon171@gmail.com>
2022-08-10 15:17:01 +0700

    fix3

--- src/Layout/PlayerControl/index.js
+++ src/Layout/PlayerControl/index.js
@@ -1,86 +1,140 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { trackContext } from "../../App";
 
-import MediaItem from "../../component/MediaItem";
-import Controls from "../../component/Controls";
-import MoreMenu from "../../component/MoreMenu/MoreMenu";
+import MediaItem from '../../component/MediaItem';
+import Controls from '../../component/Controls';
 
-import { GiMicrophone } from "react-icons/gi";
-import { FaWindowRestore } from "react-icons/fa";
-import { FiMoreHorizontal } from "react-icons/fi";
-import { TbVolume } from "react-icons/tb";
+// import { TbVolume } from 'react-icons/tb';
+import { GiMicrophone } from 'react-icons/gi';
+import { FaWindowRestore, FaMicrophoneAlt } from 'react-icons/fa';
+import { FiMoreHorizontal } from 'react-icons/fi';
+import { AiFillHeart, AiOutlineDownload, AiOutlineStop, AiOutlinePlusCircle,AiOutlineComment, AiOutlineLink, AiOutlineRight  } from 'react-icons/ai';
+import { BsHeadphones } from 'react-icons/bs';
+import {MdQueueMusic} from 'react-icons/md';
+import { IoMdWifi } from 'react-icons/io';
+// import {AiOutlineComment, AiOutlineLink, AiOutlineRight} from 'react-icons/ai';
+import {BiShare,BiMessageSquareAdd} from 'react-icons/bi';
+import {TbPlaylist,TbVolume} from 'react-icons/tb';
 
+import './playerControl.scss'
 import { useMusic } from "../../hooks/useMusic";
+import Button from "../../component/Button/Button";
 import UseApi from "../../API/UseApi";
+import { Link } from "react-router-dom";
 import Comment from "../../component/Comment";
 
-import "./playerControl.scss";
+// import imgf from "/.img/flexincirkleK.jpg"
 
-import { GiMicrophone } from "react-icons/gi";
-import { FaWindowRestore } from "react-icons/fa";
-import { FiMoreHorizontal } from "react-icons/fi";
-import { TbVolume } from "react-icons/tb";
+//import Toastmenu from "../../component/Toastmenu/Toast";
+import MoreMenu from "../../component/MoreMenu/MoreMenu";
+
 
 function PlayerControl() {
-  const {
-    currentSong,
-    setShowAddSong,
-    showAddSong,
-  } = useMusic();
 
-  const addSong = async (id) => {
-    try {
-      const temp = { trackId: currentSong?.id, playlistId: id };
-      const resp = await UseApi.postSong(temp);
+    const {currentSong, playlistUser, setPlaylist,setShowAddSong,showAddSong, ShowComment,} = useMusic()
+    const [Show, setShow] = useState(false)
 
-      // setPlaylistUser([...playlistUser, resp])
-    } catch (error) {
-      console.log("error post add song: ", error);
+    const [List, setList] = useState([]);
+    let toastProperties = null;
+
+    const handleShowToastMenu = (songName, playlistName) => {
+        toastProperties = {
+            id: 1,
+            tittle: "Succes",
+            description: `${songName} added to playlist ${playlistName}` ,
+           
+        }
+
+        setList([toastProperties])
     }
-  };
 
-  return (
-    <div className="player-controls">
-      <div className="control-left">
-        <MediaItem
-          img={currentSong?.artist?.picture}
-          SongName={currentSong?.title}
-          singer={currentSong?.artist?.name}
-        />
-        <div className="control-left-more">
-          <span
-            className="control-left-3point"
-            onClick={() => setShowAddSong(!showAddSong)}
-          >
-            <FiMoreHorizontal />
-          </span>
 
-          {showAddSong && (
-            <MoreMenu
-              nameSong={currentSong?.title}
-              imgSong={currentSong.artist?.picture}
-            />
-          )}
+    const addSong = async (id) => {
+      try{ 
+        const temp = {trackId: currentSong?.id, playlistId: id};
+        const resp = await UseApi.postSong(temp);
+        
+        // setPlaylistUser([...playlistUser, resp])
+      }catch (error) {
+        console.log("error post add song: ", error);
+      }
+    }    
+
+    const addSongtoplaylist = (id) => {
+        setPlaylist(id);
+        addSong(id);
+    } 
+
+    const handleShowMenuMore = () => {
+        const modal = document.querySelector(".overlay");
+        modal.classList.add("open");
+    }
+
+    const handleHideMenuMore = () => {
+        const modal = document.querySelector(".overlay");
+        modal.classList.remove("open");
+    }
+
+    const handleShowAddPlaylist = () => {
+        const modal = document.querySelector(".choose-playlist-menu-container");
+        modal.classList.add("open1");
+        
+    }
+
+    const handleHideAddPlaylist = () => {
+        const modal = document.querySelector(".choose-playlist-menu-container");
+        modal.classList.remove("open1");
+    }
+
+    
+    return (
+        <div className='player-controls'>
+            {ShowComment && 
+                <Comment
+                    onClick={(e)=>{e.stopPropagation()}}
+                />} 
+            <div className='control-left'>
+                <MediaItem 
+                    // img='https://anhdep123.com/wp-content/uploads/2021/01/nhung-hinh-anh-hoang-hon-buon.jpg' 
+                    img= {currentSong?.artist?.picture}                   
+                    SongName={currentSong?.title}
+                    singer = {currentSong?.artist?.name}
+                />
+                <div className="control-left-more" >
+                    <span className="control-left-3point" onClick={()=>setShowAddSong(!showAddSong)}>
+                        <FiMoreHorizontal />
+                    </span>
+                    
+                    {showAddSong && <MoreMenu
+                        nameSong ={currentSong?.title} 
+                        imgSong= {currentSong.artist?.picture}
+                        />
+                    }
+                </div>
+            </div>
+            <div className='control-center'><Controls/></div>
+            <div className='control-right'>
+                <span className='control-volume'>
+                    <TbVolume />
+                </span>
+                <span className='control-micro'>
+                    <GiMicrophone />
+                </span>
+                <span className='control-restore'>
+                    {/* <span onClick={() => handleShowToastMenu()}><FaWindowRestore /></span> */}
+                    <span><FaWindowRestore /></span>
+                    
+                </span>
+            </div>
+
+
+        {/* <Toastmenu toastlist={List} setList= {setList}/> */}
+
+
+            
         </div>
-      </div>
-      <div className="control-center">
-        <Controls />
-      </div>
-      <div className="control-right">
-        <span className="control-volume">
-          <TbVolume />
-        </span>
-        <span className="control-micro">
-          <GiMicrophone />
-        </span>
-        <span className="control-restore">
-          <span>
-            <FaWindowRestore />
-          </span>
-        </span>
-      </div>
 
-    </div>
-  );
+    );
 }
 
 export default PlayerControl;
